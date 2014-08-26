16ae9d4a62531fa1be82ef05dc62c07ead423fb1
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-08-26 16:41:14 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [asset-minifier "0.1.4"]]
+                 [asset-minifier "0.1.4"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
   :eval-in-leiningen true)
