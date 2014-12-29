229d1acc8055b172f1b8ef12522e76733286032d
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-12-29 11:25:14 -0500

    updated asset-minifier dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-asset-minifier "0.2.1"
+(defproject lein-asset-minifier "0.2.2"
   :description "plugin for CSS/Js asset minification"
   :url "https://github.com/yogthos/lein-asset-minifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [asset-minifier "0.1.5"]
+                 [asset-minifier "0.1.6"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
   :eval-in-leiningen true)
