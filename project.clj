e4ebd764551688c6f5d1fbb660d163b97d4fa6f2
Yogthos <yogthos@gmail.com>
2025-10-07 15:41:09 -0400

    bump up asset-minifier

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-asset-minifier "0.4.8"
+(defproject lein-asset-minifier "0.4.9"
   :description "plugin for CSS/Js asset minification"
   :url "https://github.com/yogthos/lein-asset-minifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [asset-minifier "0.2.8"]]
+                 [asset-minifier "0.2.9"]]
   :eval-in-leiningen true)
